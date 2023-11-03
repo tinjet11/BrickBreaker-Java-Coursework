d05b848f562259f397e6fe71f69ec87ee4f7364c
tinjet <tinjet1016@gmail.com>
2023-11-03 20:28:10 +0800

    Function Moved from Main.java

---
+++ src/main/java/brickGame/Init.java
@@ -0,0 +1,62 @@
+package brickGame;
+
+import javafx.scene.image.Image;
+import javafx.scene.paint.ImagePattern;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Rectangle;
+
+import java.util.Random;
+
+import static brickGame.Main.*;
+
+public class Init {
+    //set up the brick in the game according to the current level
+    public void initBoard() {
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < level + 1; j++) {
+                int r = new Random().nextInt(500);
+                if (r % 5 == 0) {
+                    continue;
+                }
+                int type;
+                if (r % 10 == 1) {
+                    type = Block.BLOCK_CHOCO;
+                } else if (r % 10 == 2) {
+                    if (!isExistHeartBlock) {
+                        type = Block.BLOCK_HEART;
+                        isExistHeartBlock = true;
+                    } else {
+                        type = Block.BLOCK_NORMAL;
+                    }
+                } else if (r % 10 == 3) {
+                    type = Block.BLOCK_STAR;
+                } else {
+                    type = Block.BLOCK_NORMAL;
+                }
+                blocks.add(new Block(j, i, colors[r % (colors.length)], type));
+                //System.out.println("colors " + r % (colors.length));
+            }
+        }
+    }
+
+    public void initBall() {
+        Random random = new Random();
+        xBall = random.nextInt(sceneWidth) + 1;
+        yBall = random.nextInt(sceneHeight - 200) + ((level + 1) * Block.getHeight()) + 15;
+        ball = new Circle();
+        ball.setRadius(ballRadius);
+        ball.setFill(new ImagePattern(new Image("ball.png")));
+    }
+
+    public void initBreak() {
+        rect = new Rectangle();
+        rect.setWidth(breakWidth);
+        rect.setHeight(breakHeight);
+        rect.setX(xBreak);
+        rect.setY(yBreak);
+
+        ImagePattern pattern = new ImagePattern(new Image("block.jpg"));
+
+        rect.setFill(pattern);
+    }
+}
