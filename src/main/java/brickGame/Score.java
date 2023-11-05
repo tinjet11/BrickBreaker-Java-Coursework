0df669c7b19733199b22a1b180326ea36cafd0b1
tinjet <tinjet1016@gmail.com>
2023-11-05 22:04:10 +0800

    refactor the function of showing score

--- src/main/java/brickGame/Score.java
+++ src/main/java/brickGame/Score.java
@@ -10,168 +10,74 @@ import javafx.animation.KeyFrame;
 import javafx.animation.KeyValue;
 import javafx.animation.Timeline;
 import javafx.util.Duration;
-
 public class Score {
-    private final Object lock = new Object(); // Lock to synchronize access to the show method
+    private Main main;
+    private static final Duration ANIMATION_DURATION = Duration.seconds(1);
+    private static final double OPACITY_END_VALUE = 0.0;
+    private static final double SCALE_END_VALUE = 0.0;
 
-    public void show(final double x, final double y, int score, final Main main) {
-        String sign;
-        if (score >= 0) {
-            sign = "+";
-        } else {
-            sign = "";
-        }
-        final Label label = new Label(sign + score);
+    public Score(Main main) {
+        this.main = main;
+    }
+
+    public void show(double x, double y, int score) {
+        String sign = (score >= 0) ? "+" : "";
+        showMessage(sign + score, x, y);
+    }
+
+    public void showMessage(String message, double x, double y) {
+        Label label = new Label(message);
         label.setTranslateX(x);
         label.setTranslateY(y);
 
-        // Synchronize access to the show method to prevent concurrent modification
-        synchronized (lock) {
-            Platform.runLater(new Runnable() {
-                @Override
-                public void run() {
-                    main.root.getChildren().add(label);
-                }
-            });
-        }
+        Platform.runLater(() -> main.root.getChildren().add(label));
 
-        // Create a Timeline for the animation
         Timeline timeline = new Timeline();
-
-        // Create KeyFrames for opacity and scale animations
-        KeyFrame opacityFrame = new KeyFrame(
-                Duration.seconds(1), // Duration for the animation
-                new KeyValue(label.opacityProperty(), 0) // End value for opacity
-        );
-
-        KeyFrame scaleXFrame = new KeyFrame(
-                Duration.seconds(1), // Duration for the animation
-                new KeyValue(label.scaleXProperty(), 0) // End value for scaleX
-        );
-
-        KeyFrame scaleYFrame = new KeyFrame(
-                Duration.seconds(1), // Duration for the animation
-                new KeyValue(label.scaleYProperty(), 0) // End value for scaleY
-        );
-
-        // Add key frames to the timeline
+        KeyFrame opacityFrame = new KeyFrame(ANIMATION_DURATION, new KeyValue(label.opacityProperty(), OPACITY_END_VALUE));
+        KeyFrame scaleXFrame = new KeyFrame(ANIMATION_DURATION, new KeyValue(label.scaleXProperty(), SCALE_END_VALUE));
+        KeyFrame scaleYFrame = new KeyFrame(ANIMATION_DURATION, new KeyValue(label.scaleYProperty(), SCALE_END_VALUE));
         timeline.getKeyFrames().addAll(opacityFrame, scaleXFrame, scaleYFrame);
-
-        // Set an event handler for when the timeline finishes
-        timeline.setOnFinished(event -> {
-            main.root.getChildren().remove(label);
-        });
-
-        // Synchronize access to the timeline to prevent concurrent modification
-        synchronized (lock) {
-            // Play the timeline to start the animation
-            Platform.runLater(new Runnable() {
-                @Override
-                public void run() {
-                    timeline.play();
-                }
-            });
-        }
-
+        timeline.setOnFinished(event -> main.root.getChildren().remove(label));
+        Platform.runLater(() -> timeline.play());
     }
 
-
-        public void showMessage(String message, final Main main) {
-            final Label label = new Label(message);
-            label.setTranslateX(220);
-            label.setTranslateY(340);
-
-            // Synchronize access to the show method to prevent concurrent modification
-            synchronized (lock) {
-                Platform.runLater(new Runnable() {
-                    @Override
-                    public void run() {
-                        main.root.getChildren().add(label);
-                    }
-                });
-            }
-
-            // Create a Timeline for the animation
-            Timeline timeline = new Timeline();
-
-            // Create KeyFrames for opacity and scale animations
-            KeyFrame opacityFrame = new KeyFrame(
-                    Duration.seconds(1), // Duration for the animation
-                    new KeyValue(label.opacityProperty(), 0) // End value for opacity
-            );
-
-            KeyFrame scaleXFrame = new KeyFrame(
-                    Duration.seconds(1), // Duration for the animation
-                    new KeyValue(label.scaleXProperty(), 0) // End value for scaleX
-            );
-
-            KeyFrame scaleYFrame = new KeyFrame(
-                    Duration.seconds(1), // Duration for the animation
-                    new KeyValue(label.scaleYProperty(), 0) // End value for scaleY
-            );
-
-            // Add key frames to the timeline
-            timeline.getKeyFrames().addAll(opacityFrame, scaleXFrame, scaleYFrame);
-
-            // Set an event handler for when the timeline finishes
-            timeline.setOnFinished(event -> {
-                main.root.getChildren().remove(label);
-            });
-
-            // Synchronize access to the timeline to prevent concurrent modification
-            synchronized (lock) {
-                // Play the timeline to start the animation
-                Platform.runLater(new Runnable() {
-                    @Override
-                    public void run() {
-                        timeline.play();
-                    }
-                });
-            }
-        }
-
-
-
-    public void showGameOver(final Main main) {
-        Platform.runLater(new Runnable() {
-            @Override
-            public void run() {
-                Label label = new Label("Game Over :(");
-                label.setTranslateX(200);
-                label.setTranslateY(250);
-                label.setScaleX(2);
-                label.setScaleY(2);
-
-                Button restart = new Button("Restart");
-                restart.setTranslateX(220);
-                restart.setTranslateY(300);
-                restart.setOnAction(new EventHandler<ActionEvent>() {
-                    @Override
-                    public void handle(ActionEvent event) {
-                        main.restartGame();
-                    }
-                });
-
-                main.root.getChildren().addAll(label, restart);
-
-            }
-        });
+    public void showGameOver() {
+        Label label = new Label("Game Over :(");
+        label.setTranslateX(200);
+        label.setTranslateY(250);
+        label.setScaleX(2);
+        label.setScaleY(2);
+        createRestartButton();
+        main.root.getChildren().addAll(label);
     }
 
-    public void showWin(final Main main) {
-        Platform.runLater(new Runnable() {
-            @Override
-            public void run() {
-                Label label = new Label("You Win :)");
-                label.setTranslateX(200);
-                label.setTranslateY(250);
-                label.setScaleX(2);
-                label.setScaleY(2);
+    public void showWin() {
+        Label label = new Label("You Win :)");
+        label.setTranslateX(200);
+        label.setTranslateY(250);
+        label.setScaleX(3);
+        label.setScaleY(3);
 
-
-                main.root.getChildren().addAll(label);
-
-            }
+        // Platform.runLater is used for UI updates and should not throw exceptions
+        Platform.runLater(() -> {
+            main.root.getChildren().addAll(label);
         });
+
+        // Sleep for a short time to give JavaFX time to update the UI
+        try {
+            Thread.sleep(100); // Adjust the sleep time as needed
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("showwwww");
+    }
+
+    private void createRestartButton() {
+        Button restart = new Button("Restart");
+        restart.setTranslateX(220);
+        restart.setTranslateY(300);
+        restart.setOnAction(event -> main.restartGame());
+        main.root.getChildren().add(restart);
     }
 }
