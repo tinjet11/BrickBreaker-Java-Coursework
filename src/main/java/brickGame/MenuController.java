12b2cd680d070b20c061bac549eb1e3e55306b3b
tinjet <tinjet1016@gmail.com>
2023-11-05 22:06:24 +0800

    minor change, remove unused import statement

--- src/main/java/brickGame/MenuController.java
+++ src/main/java/brickGame/MenuController.java
@@ -1,22 +1,12 @@
 package brickGame;
 
-import javafx.fxml.FXML;
 
+import javafx.fxml.FXML;
 import javafx.scene.control.Button;
-import javafx.stage.Stage;
 
 public class MenuController {
-    Main main = new Main();
-    @FXML private Button startButton;
-    @FXML private Button exitButton;
-
     @FXML
     public void exit(){
         System.exit(0);
     }
-
-
-
-
-
 }
