ddae7cb6e8eed832451cbb0fce8a89cc36464b40
tinjet <tinjet1016@gmail.com>
2023-11-05 22:08:05 +0800

    added new contoller for gamescene (to be implement)

---
+++ src/main/java/brickGame/GameSceneController.java
@@ -0,0 +1,10 @@
+package brickGame;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+public class GameSceneController {
+
+}
