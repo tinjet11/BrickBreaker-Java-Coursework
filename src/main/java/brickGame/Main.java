86b4f3f852dea8bb684bddd672ee52e6809b3443
tinjet <tinjet1016@gmail.com>
2023-11-20 00:05:18 +0800

    variable name refactoring, move nextlevel() to GameStateManager.java

--- src/main/java/brickGame/Main.java
+++ src/main/java/brickGame/Main.java
@@ -4,10 +4,8 @@ import javafx.application.Application;
 import javafx.application.Platform;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
-import javafx.scene.paint.ImagePattern;
 import javafx.scene.shape.Circle;
 import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
@@ -15,24 +13,25 @@ import javafx.stage.Stage;
 import java.io.*;
 import java.util.ArrayList;
 
-import java.util.Random;
-
 import static brickGame.BallControl.*;
 
 public class Main extends Application {
 
     public static boolean isGameRun = false;
+
+
     public static int endLevel = 18;
+
     public static int level = 1;
 
     public static double xPaddle = 0.0f;
     public static double centerPaddleX;
     public static double yPaddle = 640.0f;
-    public static final int paddleWidth = 130;
-    public static final int paddleHeight = 10;
-    public static final int halfPaddleWidth = paddleWidth / 2;
-    public static final int sceneWidth = 500;
-    public static final int sceneHeight = 700;
+    public static final int PADDLE_WIDTH = 130;
+    public static final int PADDLE_HEIGHT = 10;
+    public static final int HALF_PADDLE_WIDTH = PADDLE_WIDTH / 2;
+    public static final int SCENE_WIDTH = 500;
+    public static final int SCENE_HEIGHT = 700;
     public static final int LEFT = 1;
     public static final int RIGHT = 2;
     public static Circle ball;
@@ -43,7 +42,7 @@ public class Main extends Application {
     public static boolean isExistHeartBlock = false;
 
     public static Rectangle rect;
-    public static final int ballRadius = 10;
+    public static final int BALL_RADIUS = 10;
 
     public static int remainingBlockCount = 0;
 
@@ -59,10 +58,10 @@ public class Main extends Application {
     public static GameEngine engine;
 
     public static Scene gameScene;
-    public static final String savePathDir = "save"; // Relative to the project directory
+    public static final String SAVE_PATH_DIR = "save"; // Relative to the project directory
 
     // Construct the complete path using the directory and filename
-    public static final String savePath = savePathDir + "/save.mdds";
+    public static final String SAVE_PATH = SAVE_PATH_DIR + "/save.mdds";
 
     public static ArrayList<Block> blocks = new ArrayList<>();
     public static ArrayList<Bonus> chocos = new ArrayList<>();
@@ -131,49 +130,5 @@ public class Main extends Application {
         launch(args);
     }
 
-    private boolean nextLevelInProgress = false;
 
-    public void nextLevel() {
-        // Check if nextLevel is already in progress, if yes, return
-        if (nextLevelInProgress) {
-            return;
-        }
-
-        // Set the flag to indicate that nextLevel is in progress
-        nextLevelInProgress = true;
-
-        Platform.runLater(() -> {
-            try {
-                vX = 1.000;
-                vY = 1.000;
-
-                engine.stop();
-                resetcollideFlags();
-                goDownBall = true;
-
-                isGoldStatus = false;
-                isExistHeartBlock = false;
-
-                hitTime = 0;
-                time = 0;
-                goldTime = 0;
-
-                engine.stop();
-                blocks.clear();
-                chocos.clear();
-                remainingBlockCount = 0;
-                if (level < endLevel) {
-                    level++;
-                }
-                startGame(primaryStage);
-
-            } catch (Exception e) {
-                e.printStackTrace();
-                System.out.printf("%s", "nextLevel function in Main.java:");
-            } finally {
-                // Reset the flag to indicate that nextLevel is completed
-                nextLevelInProgress = false;
-            }
-        });
-    }
 }
