29111c8d8278b0c4cc35b58eb08bc39bfe1fe1da
tinjet <tinjet1016@gmail.com>
2023-12-01 21:24:22 +0800

    update module info

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,4 +5,20 @@ module brickGame {
 
     opens brickGame to javafx.fxml;
     exports brickGame;
+    exports brickGame.controller;
+    opens brickGame.controller to javafx.fxml;
+    exports brickGame.model.dropitem;
+    opens brickGame.model.dropitem to javafx.fxml;
+    exports brickGame.handler.blockhit;
+    opens brickGame.handler.blockhit to javafx.fxml;
+    exports brickGame.factory.blockhithandler;
+    opens brickGame.factory.blockhithandler to javafx.fxml;
+    exports brickGame.factory;
+    opens brickGame.factory to javafx.fxml;
+    exports brickGame.model;
+    opens brickGame.model to javafx.fxml;
+    exports brickGame.handler;
+    opens brickGame.handler to javafx.fxml;
+    exports brickGame.serialization;
+    opens brickGame.serialization to javafx.fxml;
 }
